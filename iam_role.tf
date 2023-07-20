505a157ce6ebcdd8561e2ea8da34b8c495d90640
swathidhanu <81679244+Swathi0123@users.noreply.github.com>
2023-07-20 18:21:56 +0530

    Update iam_role.tf

--- iam_role.tf
+++ iam_role.tf
@@ -37,7 +37,6 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
 
 resource "aws_iam_role" "worker781" {
   name = "ed-eks-worker781"
-  role = aws_iam_role.master781.name
 
   assume_role_policy = <<POLICY
 {
