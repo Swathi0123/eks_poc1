92947f1989c47481e7be584b1535a4621a490d73
Swathi <swathidhanu0697@gmail.com>
2023-07-20 17:24:58 +0530

    1st commit

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.66.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket         = "poceks1"
+    key            = "terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "poceks1"
+  }
+}
